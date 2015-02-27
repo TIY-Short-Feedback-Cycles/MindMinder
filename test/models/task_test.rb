746cd65168ee020efc97eebeca881ac0a0431e96
C Haden <chaden@spoonflower.com>
2015-02-27 10:29:13 -0500

    sweet unit test

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -4,4 +4,12 @@ class TaskTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+
+
+  test "tasks exist" do
+    assert tasks(:one)
+    assert_equal "Tasks!", tasks(:three).description
+  end
+  
 end
